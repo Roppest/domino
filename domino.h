5743b2fecb5e757dce020014e2e9d9c4a0ba7897
Rodrigo Vazquez <rodvaudio@gmail.com>
2018-02-07 22:03:09 -0600

    Create domino.h

---
+++ domino.h
@@ -0,0 +1,14 @@
+struct ficha
+{
+  int num1;
+  int num2;
+};
+
+
+void jugar();
+void iniFichas();
+void printFichas();
+void revolverFichas();
+void repartirFichas();
+void printJugadores();
+void pasarTurno();
